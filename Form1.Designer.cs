32d3893fddd407bcd86ea711971a73755cce0eb8
77jodie77 <44469498+77jodie77@users.noreply.github.com>
2019-11-10 14:36:06 +0000

    Added WindowsFormsApp1

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class ATM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(89, 166);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(164, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(89, 34);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(48, 38);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = ">";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.optionBtn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(89, 78);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(48, 38);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = ">";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.optionBtn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(89, 122);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(48, 38);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = ">";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.optionBtn_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(143, 47);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(35, 13);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "label1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(143, 91);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(35, 13);
+            this.lbl2.TabIndex = 5;
+            this.lbl2.Text = "label2";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(143, 135);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(35, 13);
+            this.lbl3.TabIndex = 6;
+            this.lbl3.Text = "label3";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(259, 156);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(61, 39);
+            this.btnEnter.TabIndex = 8;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // ATM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 261);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "ATM";
+            this.Text = "ATM";
+            this.Load += new System.EventHandler(this.ATM_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
